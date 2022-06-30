f6a492d11f9c38bbe95ac35af3087593dbf43de4
Dhefu Upadhyay <Dhefu Upadhyay>
2022-06-30 23:16:17 +0530

    added project

---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import playStore from '../../../images/playstore.png'
+import appStore from '../../../images/Appstore.png'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer id='footer'>
+      <div className='midFooter'>
+        <h1>BUYERSBAY.</h1>
+        <p>High Quality is our first priority</p>
+
+        <p>Copyrights 2021 &copy; BuyersBay</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
